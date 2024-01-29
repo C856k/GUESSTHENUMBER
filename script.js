3c93d67e5355d4e98b425e2807cd16219098e6e2
C856k <Carl856k@stud.kea.dk>
2024-01-29 14:53:22 +0100

    made responses

--- script.js
+++ script.js
@@ -2,30 +2,50 @@
 
 window.addEventListener("load", start);
 
+let num;
+
 function start(){
     console.log("JavaScript is running!")
+    num = generateRandomNumber();
+    document.querySelector("#guess-form").
+    addEventListener("submit", recieveGuess)
 }
 
 function generateRandomNumber(){
-
+    return 42;
 }
 
-function recieveGuess(){
+function recieveGuess(event){
+    event.preventDefault();
 
+    const form = event.target;
+    const value = form.guess.value;
+    console.log("Recieved guess")
+    console.log(value)
+    checkGuess(value)
 }
 
-function checkGuess(){
-
+function checkGuess(guess){
+    if(guess === num){
+        guessIsCorrect(guess)
+    } else if(guess < num) {
+        guessIsTooLow(guess)
+    } else {
+        guessIsTooHigh(guess)
+    }
 }
 
 function guessIsCorrect(){
-
+    console.log("No fooling you :)")
 }
 
 function guessIsTooLow(){
-
+    console.log("low")
 }
 
-function guessIsTooHigh(){
-    
+function guessIsTooHigh(guess){
+    console.log("high")
+    const list = document.querySelector("#guess-list");
+    const html = `<li>You guessed ${guess} - that was too high! try again</li>`
+    list.insertAdjacentHTML("beforeend", html);
 }
