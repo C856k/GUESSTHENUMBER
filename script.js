d6a02a713f367f93b1737b0303608e050faf6867
C856k <Carl856k@stud.kea.dk>
2024-01-29 14:07:03 +0100

    Began working on JS

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ window.addEventListener("load", start);
 function start(){
     console.log("JavaScript is running!")
 }
+
+function generateRandomNumber(){
+
+}
+
+function recieveGuess(){
+
+}
+
+function checkGuess(){
+
+}
+
+function guessIsCorrect(){
+
+}
+
+function guessIsTooLow(){
+
+}
+
+function guessIsTooHigh(){
+    
+}
